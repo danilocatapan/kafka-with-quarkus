98eb570c4c7dab21aa8672b5fc67ce74940cc0aa
Danilo <danilo.foton@foton.la>
2021-08-26 11:27:19 -0300

    Separado por modulos kafka consumer, producer e streams

---
+++ kafka-producer/src/main/java/org/acme/Movie.java
@@ -0,0 +1,31 @@
+package org.acme;
+
+public class Movie {
+
+    public Movie() {
+    }
+
+    public Movie(String title, int year) {
+        this.title = title;
+        this.year = year;
+    }
+
+    String title;
+    int year;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+}
