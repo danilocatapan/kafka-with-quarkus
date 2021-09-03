55786411b2f69312518b841aed2ac3d4f8aee996
Danilo <danilo.foton@foton.la>
2021-09-03 09:41:36 -0300

    removido a estrutura de modulos para facilitar os testes

---
+++ src/main/java/org/acme/resource/MovieResource.java
@@ -0,0 +1,26 @@
+package org.acme.resource;
+
+import org.acme.mongodb.Movie;
+import org.acme.service.MovieService;
+
+import javax.inject.Inject;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import java.util.List;
+
+@Path("/movies")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
+public class MovieResource {
+
+    @Inject
+    MovieService movieService;
+
+    @GET
+    public List<Movie> list() {
+        return movieService.list();
+    }
+}
